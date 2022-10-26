acdc651aa766120f2bc71cb4d68a0c59ee01e650
chunxu <41453752+chunxu@users.noreply.github.com>
2022-10-25 19:26:27 -0500

    simplest solution with library

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -15,3 +15,12 @@
 # Output: 2.50000
 # Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5..
 
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        import numpy as np
+        return np.median(nums1+nums2)
