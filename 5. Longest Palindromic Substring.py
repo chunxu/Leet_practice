dd9dec11b649961c00911275ea3ca34c6b8e0790
chunxu <41453752+chunxu@users.noreply.github.com>
2022-10-28 21:39:46 -0500

    Update 5. Longest Palindromic Substring.py

--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -20,3 +20,26 @@
 # 1 <= s.length <= 1000
 # s consist of only digits and English letters.
 
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # Return if string is empty
+        if not s: return s
+        #set the longest palindromic substring as res
+        res = ""
+        #go through the list and i as the beginning letter in substring
+        for i in range(len(s)):
+            # go through the rest of string and set j as the end letter of substring
+            j = i + 1
+            # While j is less than length of string
+            # AND res is *not* longer than substring s[i:]
+            while j <= len(s) and len(res) <= len(s[i:]):
+                # If substring s[i:j] is a palindrome s[i:j] == s[i:j][::-1]
+                # AND substring is longer than current lenth of res
+                if s[i:j] == s[i:j][::-1] and len(s[i:j]) > len(res):
+                    res = s[i:j]
+                j += 1
+        return res
