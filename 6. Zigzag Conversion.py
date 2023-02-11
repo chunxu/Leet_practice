f325f10d4cb6de34dac20f46842584ad0a112074
chunxu <41453752+chunxu@users.noreply.github.com>
2023-02-11 00:17:28 -0600

    Update 6. Zigzag Conversion.py

--- 6. Zigzag Conversion.py
+++ 6. Zigzag Conversion.py
@@ -32,3 +32,19 @@
 # 1 <= numRows <= 1000
 
 
+def convert(s, numRows):
+    if numRows == 1 or numRows >= len(s):
+        return s
+    
+    rows = ["" for _ in range(numRows)]
+    index, step = 0, 1
+    
+    for char in s:
+        rows[index] += char
+        if index == 0:
+            step = 1
+        elif index == numRows - 1:
+            step = -1
+        index += step
+    
+    return "".join(rows)
