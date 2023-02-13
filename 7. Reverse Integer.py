9d99d3e9b91cb1413da3e083edda4ff504ba7334
chunxu <41453752+chunxu@users.noreply.github.com>
2023-02-12 21:08:32 -0600

    Update 7. Reverse Integer.py

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -23,3 +23,24 @@
 
 # -231 <= x <= 231 - 1
 
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        sign = -1 if x < 0 else 1
+        x *= sign
+        reversed_x = 0
+        while x > 0:
+            reversed_x = reversed_x * 10 + x % 10
+            x = x // 10
+        reversed_x *= sign
+        if reversed_x < -2**31 or reversed_x > 2**31 - 1:
+            return 0
+        return reversed_x
+
+# This implementation first checks if the input integer x is negative and stores the sign in the sign variable. 
+# Then it reverses the digits of x and stores the result in reversed_x. 
+# Finally, it checks if reversed_x is within the range of a signed 32-bit integer, and returns 0 if it's not, or reversed_x if it is.
+
